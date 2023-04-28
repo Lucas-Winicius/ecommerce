e02fb599782788ca3a8bb238adbf7c6100766963
Lucas-Winicius <lucasclucas7@gmail.com>
2023-04-28 10:49:08 -0300

    Get user by cookie

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -36,6 +36,51 @@ class User {
       .status(responseObj.status)
       .json(responseObj);
   }
+
+  async get(req, res) {
+    console.log(req.hostname);
+    function errorMessage() {
+      return res
+        .cookie("jwt", "", { httpOnly: true, maxAge: 1 })
+        .status(404)
+        .json({
+          success: true,
+          status: 404,
+          message: "User not found",
+          secondaryMessage:
+            "Oops, it seems like the user you are looking for has mysteriously disappeared, maybe ran away to a deserted island. 🏝️🕵️‍♀️ Please try again",
+          error: {},
+        });
+    }
+
+    const cookie = req.cookies?.jwt;
+    let jwt_obj;
+    let user;
+
+    if (!cookie) return errorMessage();
+
+    try {
+      jwt_obj = jwt.decode(cookie, process.env.JWT_SECRET);
+      user = await prisma.user.findFirst({
+        where: { email: jwt_obj.email, password: jwt_obj.password },
+      });
+    } catch (e) {
+      const err = handlers.onError(e);
+      return res.status(err.status).json(err);
+    }
+
+    if (!user) return errorMessage();
+
+    const responseObj = handlers.onSuccess({
+      status: 200,
+      message: "User found.",
+      secondaryMessage:
+        "Congratulations! We found your lost unicorn, err... we mean user! 🦄🕵️‍♀️ Welcome back!",
+      data: user,
+    });
+
+    res.status(200).json(responseObj);
+  }
 }
 
 export default new User();
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,5 +3,6 @@ import { Router } from "express";
 const router = new Router();
 
 router.post("/user", controller.post);
+router.get("/user", controller.get);
 
 export default router;
