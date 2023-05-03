a5d41d9e5684e2ef69ecfbfe5af987df12a6ceab
Lucas-Winicius <lucasclucas7@gmail.com>
2023-05-02 22:53:03 -0300

    Login check implemented

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,6 @@
 import controllerProducts from "../controllers/products.js";
 import controllerProduct from "../controllers/product.js";
+import checkLogin from "../middleware/checkLogin.js";
 import { Router } from "express";
 const router = new Router();
 
@@ -7,7 +8,7 @@ router.get("/products", controllerProducts.get);
 router.post("/products", controllerProducts.post);
 
 router.get("/product/:id", controllerProduct.get);
-router.delete("/product/:id", controllerProduct.delete);
-router.patch("/product/:id", controllerProduct.patch);
+router.delete("/product/:id", checkLogin.verify, controllerProduct.delete);
+router.patch("/product/:id", checkLogin.verify, controllerProduct.patch);
 
 export default router;
